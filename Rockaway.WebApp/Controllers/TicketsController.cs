819814bd45f0410668f0e8afbe6f21d3180f99a6
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-02 11:39:15 +0100

    Wipe projects after CPH in preparation for overhaul

--- Rockaway.WebApp/Controllers/TicketsController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Rockaway.WebApp.Controllers {
-	public class TicketsController : Controller {
-		public IActionResult Index() {
-			return View();
-		}
-	}
-}
